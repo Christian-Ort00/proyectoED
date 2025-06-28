5cab0f0daea1c64da149d904f7d1ee95ae36ef3b
Christian-Ort00 <cortega20867@ufide.ac.cr>
2025-06-28 17:03:22 -0600

    Creacion del Proyecto

---
+++ juego/src/main/java/proyecto/juego/Juego.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package proyecto.juego;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Juego {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
