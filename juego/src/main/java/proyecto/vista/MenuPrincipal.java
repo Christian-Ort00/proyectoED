458cad9d5e64d0e35911aac6a1c2fefd44d0ec8a
kasandra22 <mkasandra709@gmail.com>
2025-08-18 00:25:14 -0600

    Update MenuPrincipal.java
    Codigo comentado

--- juego/src/main/java/proyecto/vista/MenuPrincipal.java
+++ juego/src/main/java/proyecto/vista/MenuPrincipal.java
@@ -7,18 +7,18 @@ import java.awt.event.ActionEvent;
 public class MenuPrincipal extends JFrame {
 
     public MenuPrincipal() {
-        setTitle("OverCooked-Fide - Menú Principal");
-        setSize(400, 300);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("OverCooked-Fide - Menú Principal"); //Titulo
+        setSize(400, 300); // Tamaño de la ventana
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Al salir de la ventana el programa se cierra 
         setLocationRelativeTo(null);
         getContentPane().setBackground(Color.decode("#f4f4f4"));
-        setLayout(new BorderLayout());
+        setLayout(new BorderLayout());  // Define el diseño
 
         // Título
         JLabel titulo = new JLabel("OverCooked-Fide", SwingConstants.CENTER);
-        titulo.setFont(new Font("Segoe UI", Font.BOLD, 24));
-        titulo.setForeground(Color.decode("#2d3436"));
-        add(titulo, BorderLayout.NORTH);
+        titulo.setFont(new Font("Segoe UI", Font.BOLD, 24)); // Fuente y tamaño
+        titulo.setForeground(Color.decode("#2d3436")); // Color del texto
+        add(titulo, BorderLayout.NORTH); //Ubicación
 
         // Panel de botones
         JPanel panelBotones = new JPanel(new GridLayout(2, 1, 10, 10));
@@ -28,14 +28,17 @@ public class MenuPrincipal extends JFrame {
         JButton btnJugar = new JButton("Empezar Juego");
         JButton btnSalir = new JButton("Salir");
 
+        //Fuentes de los botones 
         Font fontBoton = new Font("Segoe UI", Font.PLAIN, 16);
         btnJugar.setFont(fontBoton);
         btnSalir.setFont(fontBoton);
 
+        //Estilos del botón jugar
         btnJugar.setBackground(Color.decode("#74b9ff"));
         btnJugar.setForeground(Color.WHITE);
         btnJugar.setFocusPainted(false);
 
+        //Estilos del botón salir
         btnSalir.setBackground(Color.decode("#d63031"));
         btnSalir.setForeground(Color.WHITE);
         btnSalir.setFocusPainted(false);
@@ -47,12 +50,14 @@ public class MenuPrincipal extends JFrame {
         });
 
         btnSalir.addActionListener((ActionEvent e) -> {
-            System.exit(0);
+            System.exit(0); // Cierra el programa por completo
         });
 
+        //Agregar los botones al panel
         panelBotones.add(btnJugar);
         panelBotones.add(btnSalir);
 
+        //Los botones se colocan al centro
         add(panelBotones, BorderLayout.CENTER);
         setVisible(true);
     }
