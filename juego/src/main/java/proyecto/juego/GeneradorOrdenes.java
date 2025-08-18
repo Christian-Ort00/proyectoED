a4ec7748242eb8eb1dfd4d7c6b920861f4c1609d
kasandra22 <mkasandra709@gmail.com>
2025-08-18 01:21:37 -0600

    Update GeneradorOrdenes.java
    Codigo comentado

--- juego/src/main/java/proyecto/juego/GeneradorOrdenes.java
+++ juego/src/main/java/proyecto/juego/GeneradorOrdenes.java
@@ -8,6 +8,7 @@ package proyecto.juego;
  *
  * @author Usuario
  */
+// Clase que genera órdenes de hamburguesas cada cierto tiempo
 public class GeneradorOrdenes implements Runnable{
     private Orden ordenes;
     
@@ -18,17 +19,19 @@ public class GeneradorOrdenes implements Runnable{
     
     @Override
     public void run(){
+         // Ciclo infinito para estar generando órdenes
         while(true){
             try{
-                Thread.sleep(20000);
+                Thread.sleep(20000); // Espera 20 segundos antes de generar nuevas órdenes
                 
                 while(!ordenes.estaLlena()){
-                    Hamburguesa nueva = Hamburguesa.generarAleatoria();
-                    ordenes.encolar(nueva);
-                    System.out.println("Nueva orden generada: "+ nueva);
+                    Hamburguesa nueva = Hamburguesa.generarAleatoria();   // Se crea una hamburguesa aleatoria
+                    ordenes.encolar(nueva); // Se agrega la hamburguesa a la cola de órdenes
+                    System.out.println("Nueva orden generada: "+ nueva);  // Mensaje en consola para saber que se creó la orden
                 }
                 
             }catch(InterruptedException e){
+                // Si el hilo es interrumpido, se muestra un mensaje y se rompe el ciclo
                 System.out.println("Hilo Interrumpido");
                 break;
             }
