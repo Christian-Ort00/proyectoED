c6c98f479408f87ebd6f227f64d022551061ec3f
kasandra22 <mkasandra709@gmail.com>
2025-08-18 01:26:12 -0600

    Update NodoOrden.java
    Codigo comentado

--- juego/src/main/java/proyecto/juego/NodoOrden.java
+++ juego/src/main/java/proyecto/juego/NodoOrden.java
@@ -9,12 +9,12 @@ package proyecto.juego;
  * @author Usuario
  */
 public class NodoOrden {
-       Hamburguesa hamburguesa;
+       Hamburguesa hamburguesa;  // La hamburguesa que se guarda en este nodo
        NodoOrden siguiente;
        
        public NodoOrden(Hamburguesa hamburguesa){
-           this.hamburguesa=hamburguesa;
-           this.siguiente=null;
+           this.hamburguesa=hamburguesa; // Guardamos la hamburguesa
+           this.siguiente=null; // Al inicio no apunta a otro nodo
        }
        
        
